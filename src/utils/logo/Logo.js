616cad7c7c36062305a72313b078a871cae0717a
WYT <tina.yiting.wei@gmail.com>
2021-07-14 14:01:15 +0800

    adjust logo

--- src/utils/logo/Logo.js
+++ src/utils/logo/Logo.js
@@ -1,28 +1,28 @@
 import React from 'react';
 
 const shape1style = {
-  fill: '#9ca3af',
-  opacity: '0.65',
+  fill: '#6cacbb',
+  opacity: '0.9',
 };
 
 const shape2style = {
-  fill: '#6cacbb',
-  opacity: '0.65',
+  fill: '#9ca3af',
+  opacity: '0.75',
 };
 
 const Logo = () => {
   return (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 221.73 221.73">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 175.12 175.12">
         <g id="Layer_2" data-name="Layer 2">
           <g id="Layer_2-2" data-name="Layer 2">
             <path
               style={shape1style}
-              d="M153.7,110.86,43,221.73H193.86a27.87,27.87,0,0,0,27.87-27.87v-166A27.87,27.87,0,0,0,193.86,0H43Z"
+              d="M61.44,113.69l-.06-94.47L4.92,75.68a16.8,16.8,0,0,0,0,23.76L75.68,170.2a16.8,16.8,0,0,0,23.76,0l56.46-56.46Z"
             />
             <path
               style={shape2style}
-              d="M68,110.86,178.75,0H27.87A27.87,27.87,0,0,0,0,27.87v166a27.87,27.87,0,0,0,27.87,27.87H178.75Z"
+              d="M113.69,61.44l0,94.46L170.2,99.44a16.8,16.8,0,0,0,0-23.76L99.44,4.92a16.8,16.8,0,0,0-23.76,0L19.22,61.38Z"
             />
           </g>
         </g>
