e6bf2704e1b11bc99f063b9640396da64517505f
WYT <tina.yiting.wei@gmail.com>
2021-07-15 16:43:11 +0800

    adjust Logo icon and favicon

--- src/utils/logo/Logo.js
+++ src/utils/logo/Logo.js
@@ -5,36 +5,26 @@ const shape1style = {
 };
 
 const shape2style = {
-  fill: '#bbbec5',
-};
-
-const shape3style = {
-  fill: '#778489',
+  fill: '#9ca3af',
+  opacity: '0.65',
 };
 
 const Logo = () => {
   return (
-    <>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 175.12 175.12">
-        <g id="Layer_2" data-name="Layer 2">
-          <g id="Layer_2-2" data-name="Layer 2">
-            <path
-              style={shape1style}
-              d="M61.44,113.69l-.06-94.47L4.92,75.68a16.8,16.8,0,0,0,0,23.76L75.68,170.2a16.8,16.8,0,0,0,23.76,0l56.46-56.46Z"
-            />
-            <path
-              style={shape2style}
-              d="M113.69,61.44l0,94.46L170.2,99.44a16.8,16.8,0,0,0,0-23.76L99.44,4.92a16.8,16.8,0,0,0-23.76,0L19.22,61.38Z"
-            />
-            <polygon
-              style={shape3style}
-              points="155.9 113.74 113.72 113.72 113.74 155.9 155.9 113.74"
-            />
-            <polygon style={shape3style} points="61.4 61.4 61.38 19.22 19.22 61.38 61.4 61.4" />
-          </g>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 175.98 175.98">
+      <g id="Layer_2" data-name="Layer 2">
+        <g id="Layer_2-2" data-name="Layer 2">
+          <path
+            style={shape1style}
+            d="M62,114.34V19.48L4.62,76.84a15.76,15.76,0,0,0,0,22.3l72.22,72.22a15.76,15.76,0,0,0,22.3,0l57-57Z"
+          />
+          <path
+            style={shape2style}
+            d="M171.36,76.84,99.14,4.62a15.76,15.76,0,0,0-22.3,0L4.62,76.84a15.76,15.76,0,0,0,0,22.3L22,116.54,88,45.86,114,73V156.5l57.36-57.36A15.76,15.76,0,0,0,171.36,76.84Z"
+          />
         </g>
-      </svg>
-    </>
+      </g>
+    </svg>
   );
 };
 
